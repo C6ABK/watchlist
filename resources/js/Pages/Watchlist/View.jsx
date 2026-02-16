db2904ba35158490f7ee9d6ad247901ec1e7e9d3
Adam Bark <40392203+C6ABK@users.noreply.github.com>
2026-02-16 14:51:17 +0000

    Editing test for sqlite/mysql switch

--- resources/js/Pages/Watchlist/View.jsx
+++ resources/js/Pages/Watchlist/View.jsx
@@ -3,7 +3,7 @@ import AppLayout from "../../Layouts/AppLayout";
 export default function ViewWatchlist() {
   return (
     <AppLayout title="Your Watchlists">
-      <div className="text-2xl font-bold">Your Watchlists</div>
+      <div className="text-2xl font-bold">Your EDITED watchlists</div>
     </AppLayout>
   )
 }
